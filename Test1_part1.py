d4aa6e19728f5a1d2d02ea9c0f42e61d425385ed
Muhammad Awais <72553466+aetooc@users.noreply.github.com>
2020-12-01 18:46:19 +0500

    Adding solution no 1

---
+++ Test1_part1.py
@@ -0,0 +1,8 @@
+# My Answer for Question 1
+
+number= int(input('Enter a number:'))
+Range= int(input('Enter the Range:'))
+
+for j in range(1, Range+1):
+    k=number*j
+    print(number,'x',j,'=',k)
