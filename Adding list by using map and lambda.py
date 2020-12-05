de0ada65c3388ea632c663ba5256b3dfc57fac3f
Muhammad Awais <72553466+aetooc@users.noreply.github.com>
2020-12-05 14:56:41 +0500

    Adding output for better understanding

--- Adding list by using map and lambda.py
+++ Adding list by using map and lambda.py
@@ -8,3 +8,15 @@ print(num3)
 result=map(lambda x, y, z: x + y + z, num1, num2, num3)
 print('\nNew list after adding above three lists:')
 print(list(result))
+
+# When you will run this program then the output will look like this
+
+#Output:
+
+Original list: 
+[1, 2, 3]
+[4, 5, 6]
+[7, 8, 9]
+
+New list after adding above three lists:
+[12, 15, 18]
